f449cb49b733a1da4d72c6f31086a09a64364aa0
Misbah <103697755+misbah7172@users.noreply.github.com>
2025-01-22 09:43:52 +0600

    Update db_connect.php

--- DropEx/db_connect.php
+++ DropEx/db_connect.php
@@ -1,6 +1,6 @@
 <?php 
 	// connect to the database
-	$conn = mysqli_connect('localhost', 'root', '', 'DropEx');
+	$conn = mysqli_connect('mysql.railway.internal', 'root', 'VQudHgzjyxEniyLiwzQZPSWBRhcNkgXS', 'railway', '3306');
 	// check connection
 	if(!$conn){
 		echo 'Connection error: '. mysqli_connect_error();
